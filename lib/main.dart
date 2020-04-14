f43ca87bd424e008709e9b88057777c6386c2c2d
Marco A. Braghim <marco.a.braghim@gmail.com>
2020-04-14 10:17:37 -0300

    Android X

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:delivery_app/layout.dart';
 import 'package:delivery_app/login.dart';
 import 'package:flutter/material.dart';
-// import 'package:onesignal_flutter/onesignal_flutter.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
-void main() {
-  // OneSignal.shared.init('02d41705-3373-4da2-8c23-7e0e77f8c239');
-
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // ]
+    // Start OneSignal connection
+    OneSignal.shared.init('02d41705-3373-4da2-8c23-7e0e77f8c239');
+
+    // OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.none);
+
+    // OneSignal.shared.setNotificationReceivedHandler((OSNotification notification) {
+
+    // });
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
